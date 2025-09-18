2c748173210b313eb55c3319cc203147292f475f
HelodeLemos <heloidelemos@gmail.com>
2025-09-18 15:59:07 -0300

    Primeira alteração

---
+++ exemplo_01.py
@@ -0,0 +1,5 @@
+# crie um programa que onde o usuário digite dois valores e apareça a soma
+numero1 = int(input("Digite um numero: "))
+numero2 = int(input("Digite outro numero: "))
+soma = numero1 + numero2
+print("A soma dos valores é:",soma)
---
+++ exemplo_02.py
@@ -0,0 +1,2 @@
+#Crie um programa que o usuário digite seu nome e retorne o número dos caracteres
+print(len(input("Digite seu nome:")))
